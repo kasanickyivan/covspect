55f0c502decda197464b56d87b83419ebb3a3c22
Ivan Kasanicky <kasanicky.ivan@gmail.com>
2014-04-05 13:06:08 -0600

    rmse now counted only for one analysis

--- enkf2dx/enkf2dx_rmse.m
+++ enkf2dx/enkf2dx_rmse.m
@@ -1,11 +1,14 @@
-function RMSE = enkf2dx_rmse(X,Y,Z)
-    [nx,ny,m,r] = size(X);
+%%
+%Function count RMSE of analysis. 
+%
+%   function RMSE = enkf2dx_rmse(XA,X)
+%   XA - analysis
+%   X - true state
+function RMSE = enkf2dx_rmse(XA,X)
+    [nx,ny,nvar,r] = size(XA);
     
-    X = reshape(X,nx*ny,m,r);
-    Y = reshape(Y,nx*ny,m,r);
-    Z = reshape(Z,nx*ny,m,r);
-    
-    RMSE = zeros(2,m,r);
-    RMSE(1,:,:) = sqrt(mean((X-Y).^2,1));
-    RMSE(2,:,:) = sqrt(mean((Z-Y).^2,1));
+    XA = reshape(XA,nx*ny,nvar,r);
+    X = reshape(X,nx*ny,nvar,r);
+   
+    RMSE = sqrt(mean((XA-X).^2,1));
 end
