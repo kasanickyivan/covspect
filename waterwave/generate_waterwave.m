701cd6e5fe375f71a6928b5693552eaf335abd15
Ivan Kasanicky <kasanicky.ivan@gmail.com>
2014-04-15 15:32:33 -0600

    add new parameter

--- waterwave/generate_waterwave.m
+++ waterwave/generate_waterwave.m
@@ -4,7 +4,7 @@
 %   n - dimension of grid
 %   reps - number of replications
 %   h - initial water level 
-%   dw_min,dw_max,dh_min,dh_max - argumenst to add_rand_drop
+%   dw_min,dw_max,dh_min,dh_max, bw_min - argumenst to add_rand_drop
 %   ts - time step, argument for waterwave2 function
 %   init_ts - initial time step
 %   init_d - initial nuber of drops
@@ -14,7 +14,7 @@
 % output
 %   Y - 4d array dimension [x,y,variable,replication]
 
-function Y=generate_waterwave(n,reps,h,dw_min,dw_max,dh_min,dh_max,ts,init_ts,init_d,dper)
+function Y=generate_waterwave(n,reps,h,dw_min,dw_max,dh_min,dh_max,bdd,ts,init_ts,init_d,dper)
 
     nvar=3; %number of variables foe EnKF
     dx=1;   %argument to waterwave2
@@ -23,7 +23,7 @@ function Y=generate_waterwave(n,reps,h,dw_min,dw_max,dh_min,dh_max,ts,init_ts,in
 
     Y=zeros(n,n,nvar,reps);Y(:,:,1,1)=ones(n,n)*h;
     for init_ind = 1:init_d
-        Y(:,:,1,1)=add_rand_drop(squeeze(Y(:,:,1,1)),dw_min,dw_max,dh_min,dh_max);
+        Y(:,:,1,1)=add_rand_drop(squeeze(Y(:,:,1,1)),dw_min,dw_max,dh_min,dh_max,bdd);
         Y(:,:,:,1)=waterwave2(squeeze(Y(:,:,:,1)),dt,dx,dy,init_ts);
     end
     
@@ -33,7 +33,7 @@ function Y=generate_waterwave(n,reps,h,dw_min,dw_max,dh_min,dh_max,ts,init_ts,in
         fprintf('.');
         % adding drop
         if(mod(rep_ind,dper)==0)
-            Y(:,:,1,rep_ind)=add_rand_drop(squeeze(Y(:,:,1,rep_ind)),dw_min,dw_max,dh_min,dh_max);
+            Y(:,:,1,rep_ind)=add_rand_drop(squeeze(Y(:,:,1,rep_ind)),dw_min,dw_max,dh_min,dh_max,bdd);
             Y(:,:,:,rep_ind) = waterwave2(squeeze(Y(:,:,:,rep_ind)),dt,dx,dy,ts);
             fprintf('\n')
         end
