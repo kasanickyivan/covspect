c4c0037ad4eee5eecbc237c2df5c61f6cf19b2ec
Ivan Kasanicky <kasanicky.ivan@gmail.com>
2014-04-28 11:10:33 -0600

    parameter change

--- sh/exp_swe.sh
+++ sh/exp_swe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ts=1
-for no in 8
+ts=5
+for no in 4 8 16 32
 do
 	for N in 4 8 16
 	do
-		matlab -nosplash -nodesktop -nodisplay -r "exp_swe_enkf($N,,$no,$ts);quit" >"log/lorenz_$N_$no_$ts.log" &
+		matlab -nosplash -nodesktop -nodisplay -r "exp_swe_enkf($N,$no,$ts);quit" >"log/swe_$N_$no_$ts.log" &
 	done
 done	
