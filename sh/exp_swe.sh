4d2099703e8701a9f53b140a6036aaa29fa42f99
Ivan Kasanicky <kasanicky.ivan@gmail.com>
2014-04-28 11:18:42 -0600

    log file changed

--- sh/exp_swe.sh
+++ sh/exp_swe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ts=5
-for no in 4 8 16 32
+for no in 4
 do
 	for N in 4 8 16
 	do
-		matlab -nosplash -nodesktop -nodisplay -r "exp_swe_enkf($N,$no,$ts);quit" >"log/swe_$N_$no_$ts.log" &
+		matlab -nosplash -nodesktop -nodisplay -r "exp_swe_enkf($N,$no,$ts);quit" >"log/swe_${N}_${no}_${ts}.log" &
 	done
 done	
