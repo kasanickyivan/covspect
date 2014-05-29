aa669f6ce67d9df1314ad961960acff90b6ff4db
Ivan Kasanicky <kasanickyivan@gmail.com>
2014-05-29 17:34:40 +0200

    git creation

--- swe/waterwave_anim.m
+++ swe/waterwave_anim.m
@@ -6,18 +6,32 @@
 %       Y   :   4d array from waterwave2
 %       dt  :   time diference between samples
 %       fps :   frames pre seccond 
-function waterwave_anim(Y,dt,fps)
-    [surfplot,top,start,stop] = waterwave_initgraphics(Y);
+function waterwave_anim(Y,dt,fps,file)
+    [surfplot,top] = waterwave_initgraphics(Y);
     rep = size(Y,4);
-    nstep = 1;
-    while get(start,'value')==0 && get(stop,'value')==0 && nstep <= rep
+    set(gcf,'color','w'); % set figure background to white
+    drawnow;
+        
+    for nstep = 1:rep
         C = squeeze(abs(Y(:,:,2,nstep)) + abs(Y(:,:,3,nstep)));  % Color shows momemtum
         t = nstep*dt;
         set(surfplot,'zdata',squeeze(Y(:,:,1,nstep)),'cdata',C);
         set(top,'string',sprintf('t = %6.2f',t))
-        drawnow
-        nstep = nstep+1;
+        drawnow;
+
+        if(1)
+            frame = getframe(1);
+            im = frame2im(frame);
+            [imind,cm] = rgb2ind(im,256);
+    
+            if nstep == 1 
+                % On the first loop, create the file. In subsequent loops, append
+                imwrite(imind,cm,file,'gif','DelayTime',0,'loopcount',inf);
+            else
+                imwrite(imind,cm,file,'gif','DelayTime',0,'writemode','append');
+            end
         pause(1/fps);
+        end
     end
 end
       
